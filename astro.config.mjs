b95a52df743f57eda97d2587ad04a556c0e21794
tomochan <245827238+tom-chillout@users.noreply.github.com>
2025-11-27 14:35:14 +0900

    chore: GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // GitHub Pages setting
+    site: 'https://tom-chillout.github.io',
+    base: '/astronautical-altitude',
+});
