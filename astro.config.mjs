0d39e9483501c037fb8b06a81c1dca0bbaeee96b
tomochan <245827238+tom-chillout@users.noreply.github.com>
2025-11-27 15:27:10 +0900

    Revert "fix: domain test"
    This reverts commit 51cfe9336803d2a870dbd8ed35122bc23867d073.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     // GitHub Pages setting
     site: 'https://tom-chillout.github.io',
+    base: '/astronautical-altitude',
 });
